7d961bd860ffc47de27556020e2156dee4e081b4
Nikhil1O1 <technikue2.0@gmail.com>
2025-01-15 10:29:11 +0530

    Initial update

---
+++ mvp.js
@@ -0,0 +1,31 @@
+ 
+  function keepClickingRollDiceButton() {
+    const buttons = document.getElementsByTagName('button');
+    const xpath = '/html/body/div[1]/div[4]/div/div[2]/div/div/div[1]/div/div[2]/div[2]/div[1]/div/button';
+  
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons[i];
+      const buttonBuy = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      const buttonText = button.textContent.trim();
+
+      if(buttonText === "Another game" || (buttonText === "Start") || (buttonText === "Start Game") || (buttonText === "Enter Game") || (buttonText === "Join game")){
+        button.click();
+      }
+  
+      if (buttonBuy) {
+        buttonBuy.click();
+        break;
+      }
+  
+      else if (buttonText === "Roll the dice" || 
+          buttonText === "Roll again" || 
+          buttonText === "End turn") {
+        button.click();
+        break;
+      }
+    }
+  
+    setTimeout(keepClickingRollDiceButton, 2000); 
+  }
+  
+  keepClickingRollDiceButton();
---
+++ readme.md
@@ -0,0 +1,4 @@
+This is a WIP bot to play for  multiple players,
+The mvp gets the coins.
+
+Currently there are few bugs will update later
